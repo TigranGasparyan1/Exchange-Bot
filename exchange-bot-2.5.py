69691c571564d33507681c0cd1cb2a9d4a24cd42
Tigran Gasparyan <tigrangasparyan777@gmail.com>
2022-02-25 10:16:48 -0800

    Update exchange-bot-2.5.py

--- exchange-bot-2.5.py
+++ exchange-bot-2.5.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup as BS
 import sqlite3
 from datetime import datetime
-TOKEN = 'Your Token'
+TOKEN = '5040171237:AAEIY10mURQmP9OHvAWTchIhR6BdIF8ZQDg'
 bot = telebot.TeleBot(TOKEN)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
 
